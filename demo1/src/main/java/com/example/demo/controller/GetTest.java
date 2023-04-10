fbb7c159e1138dbafc2e3327346047ba1c9b8f49
SeongheePark <sungheepppp@gmail.com>
2023-04-10 13:51:18 +0900

    delete mapping, ResponseEntity, IoC, DI 예제

--- demo1/src/main/java/com/example/demo/controller/GetTest.java
+++ demo1/src/main/java/com/example/demo/controller/GetTest.java
@@ -2,16 +2,24 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class GetTest {
-	
+
 	// http://localhost:8080/api/hello
 	@GetMapping("/hello")
 	public String getHello() {
 		return "say hello";
 	}
+	
+	@GetMapping("/queryParam1")
+	public String queryParam(@RequestParam String name) {
+		System.out.println(name);
+		return name;
+	}
+	
 	
 }
---
+++ di_ex/src/ch01/Base64Encoder.java
@@ -0,0 +1,14 @@
+package ch01;
+
+import java.util.Base64;
+
+public class Base64Encoder implements IEncoder {
+
+	@Override
+	public String encode(String message) {
+		// 인코딩 처리 ㅡ> Base64형식으로
+		String resultEncode = Base64.getEncoder().encodeToString(message.getBytes());
+		return resultEncode;
+	}
+
+}
